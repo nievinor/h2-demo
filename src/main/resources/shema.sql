3216ac6d9b4ab3bcf8c214333bea1835a65ebd63
Dmitry <applemaycry@mail.ru>
2021-02-13 01:22:42 +0300

    изменена схема

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -25,13 +25,12 @@ INSERT INTO GENRE (genre) VALUES ( 'Computer science' );
 CREATE TABLE BOOKS (
                        id INT AUTO_INCREMENT  PRIMARY KEY,
                        name VARCHAR(250) NOT NULL,
-                       number_of_pages INT NOT NULL,
                        author_id INT,
                        genre_id INT,
                        FOREIGN KEY (author_id) REFERENCES AUTHORS,
                        FOREIGN KEY (genre_id) REFERENCES GENRE
 );
 
-INSERT INTO BOOKS (name, number_of_pages, author_id, genre_id) VALUES ( 'Crime and punishment ', 672, 2, 1 );
-INSERT INTO BOOKS (name, number_of_pages, author_id, genre_id) VALUES ( 'The Shining', 447, 1, 2 );
-INSERT INTO BOOKS (name, number_of_pages, author_id, genre_id) VALUES ( 'Pro Spring 5', 1119, 3, 3 );
+INSERT INTO BOOKS (name, author_id, genre_id) VALUES ( 'Crime and punishment ', 2, 1 );
+INSERT INTO BOOKS (name, author_id, genre_id) VALUES ( 'The Shining', 1, 2 );
+INSERT INTO BOOKS (name, author_id, genre_id) VALUES ( 'Pro Spring 5', 3, 3 );
